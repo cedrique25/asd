8ee3aeb3303936972ad473a1812d77f4f54e916b
Cedie <cedrique.cablao@gmail.com>
2021-09-07 22:32:43 +0800

    NUMBER 1

---
+++ CrudAspCore/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CrudAspCore.Models;
+using System.Data;
+using System.Data.SqlClient;
+namespace CrudAspCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        db dbop = new db();
+        string msg;
+        public IActionResult Index()
+        {
+            
+            Employee emp = new Employee();
+            emp.flag = "get";
+            DataSet ds = dbop.Empget(emp, out msg);
+            DataTable dt = new DataTable();
+
+            List<Employee> list = new List<Employee>();
+            foreach (DataRow dr in ds.Tables[0].Rows)
+            {
+                list.Add(new Employee
+                {
+                    Sr_no = Convert.ToInt32(dr["Sr_no"]),
+                    Emp_name = dr["Emp_name"].ToString(),
+                    City = dr["City"].ToString(),
+                    State = dr["State"].ToString(),
+                    Country = dr["Country"].ToString(),
+                    Department = dr["Department"].ToString()
+                });
+            }
+            return View(list);
+        }
+    }
+}
